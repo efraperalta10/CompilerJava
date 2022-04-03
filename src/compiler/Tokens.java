a4db7578318bc18f7cc8fb8ca2ea63cfecb16bfc
Moy <moy_rub@hotmail.com>
2022-04-03 15:19:12 -0500

    Clase Token con tokens a detectar con anal. lexico OK

---
+++ src/compiler/Tokens.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package compiler;
+
+/**
+ *
+ * @author Moisés
+ */
+//Clase para almacenar tokens que podemos detectar con el analizador lexico
+public enum Tokens {
+    IDENTIFICADOR,
+    PROGRAM,
+    IF,
+    ELSE,
+    FI,
+    DO,
+    UNTIL,
+    WHILE,
+    READ,
+    WRITE,
+    FLOAT,
+    INT,
+    BOOL,
+    NOT,
+    AND,
+    OR,
+    SUMA,
+    RESTA,
+    MULTIPLICACION,
+    DIVISION,
+    POTENCIA,
+    MODULO,
+    MENOR,
+    MENORIGUAL,
+    MAYOR,
+    MAYORIGUAL,
+    DIFERENTE,
+    IGUAL,
+    ASIGNACION,
+    PUNTOCOMA,
+    PUNTO,
+    COMA,
+    PAR_ABRE,
+    PAR_CIERRE,
+    LLAV_ABRE,
+    LLAV_CIERRE,
+    COMILLADOBLE,
+    COMILLASIMPLE,
+    DIAGONAL,
+    DIAGONALDOBLE
+}
